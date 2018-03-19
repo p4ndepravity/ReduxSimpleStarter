910e2c0c91fd70a6b2bc88c0bfb44179b9555cc9
Trent <trent.bullard@gmail.com>
2018-03-19 07:46:02 -0500

    implement fetch-posts action creator

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Posts Index</h1>
+      </div>
+    );
+  }
+}
+
+export default PostsIndex;
