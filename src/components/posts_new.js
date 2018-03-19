871ace9e6bff2a8231de5e912462090d5b55aadb
Trent <trent.bullard@gmail.com>
2018-03-19 12:09:34 -0500

    implement validation and error notification

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,76 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render () {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger': ''}`;
+
     return (
-      <div>
-        PostsNew!
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type={field.type}
+          {...field.input}
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render () {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          type="text"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          type="text"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          type="text"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
